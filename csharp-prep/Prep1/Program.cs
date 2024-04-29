a237b2c47c7b68269a8b751de97463f2f40be92d
Mauma-Awaji <soberemanasseh@gmail.com>
2024-04-29 17:20:29 -0600

    C# Prep 1 and C# Prep 2 completed

--- csharp-prep/Prep1/Program.cs
+++ csharp-prep/Prep1/Program.cs
@@ -5,5 +5,12 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep1 World!");
+        Console.Write("Whats is your first name? ");
+        string firstname = Console.ReadLine();
+        Console.Write("Whats is your last name? ");
+        string lastname = Console.ReadLine();
+        Console.WriteLine(" ");
+        Console.WriteLine($"Your name is {lastname}, {firstname} {lastname}.");
+
     }
 }
--- csharp-prep/Prep2/Program.cs
+++ csharp-prep/Prep2/Program.cs
@@ -5,5 +5,52 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep2 World!");
+        Console.Write("What is your grade percentage(Just put in the number): ");
+        string gradeString = Console.ReadLine();
+        int gradeInt = int.Parse(gradeString);
+        string gradeLetter = "";
+        string gradeSign = "";
+
+        if (gradeInt >= 90) 
+        { 
+            gradeLetter = "A";
+        }
+ 
+        else if (gradeInt >= 80) 
+        { 
+            gradeLetter = "B";
+        }  
+        else if (gradeInt >= 70) 
+        { 
+            gradeLetter = "C";
+        }
+        else if (gradeInt >= 60) 
+        { 
+            gradeLetter = "D";
+        }
+        else
+        { 
+            gradeLetter = "F";
+        }
+
+        if (gradeInt % 10 >= 7 && !(gradeLetter == "A" || gradeLetter == "F"))
+        {
+            gradeSign = "+";
+        } 
+        else if (gradeInt % 10 < 3 && !(gradeLetter == "A" || gradeLetter == "F"))
+        {
+            gradeSign = "-";
+        }
+
+        Console.WriteLine($"Your grade is {gradeLetter}{gradeSign}.");
+
+        if (gradeInt >= 70)
+        {
+            Console.WriteLine("Congratulations! You passed the class!");
+        }
+        else
+        {
+            Console.WriteLine("I'm sorry. Your grade isn't high enough to pass this class. Try again. I know you can do it!");
+        }
     }
 }
