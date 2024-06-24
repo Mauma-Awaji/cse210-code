cda81e7ab4b05d511a99c8b1f8947076c12faf8a
Mauma-Awaji <soberemanasseh@gmail.com>
2024-06-24 12:23:23 -0600

    Prep 3-5 All done!

--- csharp-prep/Prep3/Program.cs
+++ csharp-prep/Prep3/Program.cs
@@ -5,5 +5,34 @@ class Program
     static void Main(string[] args)
     {
         Console.WriteLine("Hello Prep3 World!");
+        string play = "yes";
+        while (play.ToLower() == "yes")
+        {
+            Random randomGenerator = new Random();
+            int mag_num = randomGenerator.Next(1, 101); 
+            int guess_num = -2000;
+            int count = 0;
+            while (guess_num != mag_num)
+            {
+                count += 1;
+                Console.Write("What is your guess? ");
+                guess_num = int.Parse(Console.ReadLine());
+                if (guess_num == mag_num)
+                {
+                    Console.WriteLine("You guessed it!");
+                }
+                else if (guess_num < mag_num)
+                {
+                    Console.WriteLine("Higher");
+                }
+                else
+                {
+                    Console.WriteLine("Lower");
+                }
+            }
+            Console.WriteLine($"It took {count} guesses");
+            Console.Write("Would you like to play again? ");
+            play = Console.ReadLine();
+        }
     }
 }
--- csharp-prep/Prep4/Program.cs
+++ csharp-prep/Prep4/Program.cs
@@ -3,7 +3,38 @@ using System;
 class Program
 {
     static void Main(string[] args)
+
+    // Aided by ChatGPT
     {
         Console.WriteLine("Hello Prep4 World!");
+        List<int> numbersList = new();
+        int number = 1;
+        Console.WriteLine("Enter a list of numbers, type 0 when finished.");
+        while (number != 0)
+        {
+            Console.Write("Enter number: ");
+            number = int.Parse(Console.ReadLine());
+            numbersList.Add(number);
+        }
+        numbersList.RemoveAt(numbersList.Count-1);
+        Console.WriteLine($"The sum is: {numbersList.Sum()}");
+        Console.WriteLine($"The average is: {numbersList.Average()}");
+                // Finding the smallest positive number
+        int? smallestPositive = null;
+        foreach (int num in numbersList)
+        {
+            if (num > 0 && (smallestPositive == null || num < smallestPositive))
+            {
+                smallestPositive = num;
+            }
+        }
+        Console.WriteLine($"The largest number positive is: {smallestPositive}");
+        numbersList.Sort();
+        Console.WriteLine("The sorted list is:");
+        foreach (int num in numbersList)
+        {
+            Console.WriteLine(num);
+        }
+
     }
 }
